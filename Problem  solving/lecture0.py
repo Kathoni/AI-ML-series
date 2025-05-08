8535b37639bf62a3827ebe2378bceb134e4d87cf
Kathoni <thrusherkeys@gmail.com>
2025-05-08 13:09:13 +0300

    what is to be covered

---
+++ Problem  solving/lecture0.py
@@ -0,0 +1,11 @@
+# Objectives
+# explore some of the ideas that make AI possible
+#search
+#Knowledge
+# reasoning
+# learning
+# Uncertainty
+# optimization
+# Neural Networks
+# Language
+# understand the basic building blocks of AI
